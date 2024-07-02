31a67749f0a687955c3b32f19dc421be71e0fee8
Mele <juantanca11@icloud.com>
2024-07-02 20:33:31 -0300

    Scripts parte A.3, A.4 y A.5

---
+++ parteA.4/parteA4.ps1
@@ -0,0 +1,30 @@
+﻿$userName = "Recepcion"
+$userProfilePath = "C:\Users\$userName"
+$restrictedFolders = "C:\Windows", "C:\Program Files", "C:\Program Files (x86)", "C:\Users\Public"
+
+# prohibimos los accesos a las carpetas
+function Deny-Access {
+    param (
+        [string]$path,
+        [string]$userName
+    )
+    $acl = Get-Acl -Path $path
+    $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("$userName", "FullControl", "Deny")
+    $acl.SetAccessRule($accessRule)
+    Set-Acl -Path $path -AclObject $acl
+}
+
+foreach ($folder in $restrictedFolders) {
+    Deny-Access -path $folder -userName $userName
+}
+
+# permitimos acceso completo a las carpetas propias del usuario
+$userFolders = Get-ChildItem -Path $userProfilePath -Directory
+foreach ($folder in $userFolders) {
+    $acl = Get-Acl -Path $folder.FullName
+    $acl.ResetAccessRuleAll()
+    $acl.SetAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule("$userName", "FullControl", "Allow")))
+    Set-Acl -Path $folder.FullName -AclObject $acl
+}
+
+Write-Host "Se han configurado las restricciones para el usuario '$userName'."
